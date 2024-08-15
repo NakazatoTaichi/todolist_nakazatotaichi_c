91d436ac2231aa173d6aee11f504e1b548c06bb0
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-08-16 04:30:11 +0900

    タスクが作成できるようにした

--- todo/forms.py
+++ todo/forms.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
+from django import forms
 from .models import CustomUser
-
+from .models import Task
+import pytz
+from datetime import datetime
+from django.core.exceptions import ValidationError
 
 class SignUpForm(UserCreationForm):
     class Meta:
@@ -13,6 +17,29 @@ class SignUpForm(UserCreationForm):
             "password2",
         )
 
-class LoginForm(AuthenticationForm):
-    pass
+class TaskForm(forms.ModelForm):
+    due_date = forms.DateTimeField(
+        required=True,
+        input_formats=['%Y-%m-%d %H:%M'],
+        label="期限",
+        initial=datetime.now(pytz.timezone('Asia/Tokyo')),
+        widget=forms.DateTimeInput(attrs={
+            'type': 'datetime-local',
+            'step': 300
+        }),
+        disabled=False
+    )
+    class Meta:
+        model = Task
+        fields = (
+            "title",
+            "due_date",
+            "memo"
+        )
 
+    def clean_due_date(self):
+        due_date = self.cleaned_data.get('due_date')
+        now = datetime.now(pytz.timezone('Asia/Tokyo'))
+        if due_date and due_date > now:
+            return due_date
+        raise ValidationError("期限は現在より未来でなければなりません。")
--- todo/models.py
+++ todo/models.py
@@ -3,3 +3,17 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email = models.EmailField('メールアドレス', unique=True)
+
+class Task(models.Model):
+    TASK_STATUS = [
+        ('not_started', '未完了'),
+        ('completed', '完了'),
+    ]
+
+    title = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=TASK_STATUS, default='not_started')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    due_date = models.DateTimeField()
+    memo = models.TextField(default='詳細なし')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
