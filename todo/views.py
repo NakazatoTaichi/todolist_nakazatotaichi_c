671b0880e0d1f8214ee13adf92ef3e2e64ccf204
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-08-24 14:57:19 +0900

    TaskのCRUD機能作成

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.views.generic import TemplateView, CreateView
 from django.urls import reverse_lazy
+from django.utils import timezone
 from django.views import View
 from .forms import SignUpForm
 from .models import Task
@@ -26,7 +27,19 @@ class SignUpView(View):
 
 @login_required
 def home(request):
-    return render(request, 'todo/home.html')
+    # 現在の日付と時刻
+    now = timezone.now()
+
+    # 現在の日付と時刻に基づいて今日のタスクと完了済みのタスクを取得
+    today_tasks = Task.objects.filter(due_date__date=now.date(), status='not_started', user=request.user)
+    completed_tasks = Task.objects.filter(status='completed', user=request.user)
+
+    context = {
+        'today_tasks': today_tasks,
+        'completed_tasks': completed_tasks
+    }
+
+    return render(request, 'todo/home.html', context)
 
 @login_required
 def task_create(request):
@@ -40,3 +53,21 @@ def task_create(request):
     else:
         form = TaskForm()
     return render(request, 'todo/task_create.html', {'form': form})
+
+@login_required
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('todo:home')
+    else:
+        form = TaskForm(instance=task)
+
+    context = {
+        'form': form,
+        'task': task
+    }
+    return render(request, 'todo/task_edit.html', context)
