fc4e9470aef0fc05b16f2abe8bc54b84ccaf3ba8
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-09-18 20:51:15 +0900

    修正８

--- todoproject/todoproject/asgi.py
+++ todoproject/todoproject/asgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoproject.settings')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoproject.todoproject.settings')
 
 application = get_asgi_application()
