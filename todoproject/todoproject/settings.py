4c212348fff9759d6e92c3b7ee4490d3634b1734
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-09-18 23:06:15 +0900

    修正

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -30,7 +30,7 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT', '3306')
+DB_PORT = os.environ.get('DB_PORT')
 
 
 ALLOWED_HOSTS = ['todolist-nakazatotaichi-c.onrender.com']
