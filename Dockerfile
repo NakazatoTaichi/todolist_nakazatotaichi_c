1f4f7f68cb2e9944754550f1c76bd60a633179f8
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-09-18 19:44:43 +0900

    修正

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /code/
 
-EXPOSE 8000
-
-ENTRYPOINT [ "gunicorn", "todoproject.wsgi:application", "--bind", "0.0.0.0:8000" ]
+ENTRYPOINT [ "gunicorn", "todoproject.wsgi" ]
--- todoproject/todoproject/wsgi.py
+++ todoproject/todoproject/wsgi.py
@@ -7,7 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
