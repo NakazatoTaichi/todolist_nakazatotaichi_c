d02b1928731b768035722d22faf49f530e10e6a8
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-09-19 16:21:30 +0900

    バグ修正

--- todo/views.py
+++ todo/views.py
@@ -106,7 +106,8 @@ def task_edit(request, pk):
             return redirect('todo:home')
     else:
         form = TaskForm(instance=task)
-        context = {
+
+    context = {
         'form': form,
         'task': task
     }
